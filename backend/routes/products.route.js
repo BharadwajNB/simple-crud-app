9dcdcaea2e9042ef54069bb95a177ddbf69d901f
BharadwajNB <bharadwajnb23@gmail.com>
2026-01-04 19:49:12 +0530

    Updated get method

--- backend/routes/products.route.js
+++ backend/routes/products.route.js
@@ -12,6 +12,21 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }       
+        res.status(200).json(product);
+    }
+    catch (error) { 
+        res.status(500).json({ message: error.message });   
+        }
+});
+
 // ADD product
 router.post('/', async (req, res) => {
     try {
